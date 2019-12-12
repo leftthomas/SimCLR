947c9fa51a39c647aa30280fc34b6aeed7cda4dc
leftthomas <leftthomas@qq.com>
2019-12-13 07:41:07 +0800

    change optimizer to SGD

--- utils.py
+++ utils.py
@@ -16,7 +16,7 @@ class DuplicatedCompose(object):
 
 
 train_transform = DuplicatedCompose([
-    transforms.RandomResizedCrop(224, scale=(0.8, 1.2), ratio=(0.8, 1.2)),
+    transforms.RandomResizedCrop(64, scale=(0.8, 1.2), ratio=(0.8, 1.2)),
     transforms.RandomApply([transforms.ColorJitter(0.4, 0.4, 0.4, 0.2)], p=0.8),
     transforms.RandomHorizontalFlip(),
     transforms.RandomGrayscale(p=0.25),
@@ -24,8 +24,8 @@ train_transform = DuplicatedCompose([
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
 
 test_transform = transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(224),
+    transforms.Resize(64),
+    transforms.CenterCrop(64),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
 

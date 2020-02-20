cae3f80543caeb670434c33b5e943aefce5910bc
leftthomas <leftthomas@qq.com>
2020-02-20 15:58:57 +0800

    finish data aug

--- utils.py
+++ utils.py
@@ -15,18 +15,19 @@ class CIFAR10Instance(datasets.CIFAR10):
         img = Image.fromarray(img)
 
         if self.transform is not None:
-            img = self.transform(img)
+            pos_1 = self.transform(img)
+            pos_2 = self.transform(img)
 
         if self.target_transform is not None:
             target = self.target_transform(target)
 
-        return img, target, index
+        return pos_1, pos_2, target
 
 
 train_transform = transforms.Compose([
-    transforms.RandomResizedCrop(32, scale=(0.2, 1.0)),
-    transforms.RandomApply([transforms.ColorJitter(0.4, 0.4, 0.4, 0.4)], p=0.8),
-    # transforms.RandomHorizontalFlip(),
+    transforms.RandomResizedCrop(32),
+    transforms.RandomHorizontalFlip(p=0.5),
+    transforms.RandomApply([transforms.ColorJitter(0.8, 0.8, 0.8, 0.2)], p=0.8),
     transforms.RandomGrayscale(p=0.2),
     transforms.ToTensor(),
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])

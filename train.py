63a9ecbc2d229c84aec7847f5ebecf73b8a01989
leftthomas <leftthomas@qq.com>
2020-02-20 16:50:27 +0800

    finish basic structure

--- train.py
+++ train.py
@@ -2,10 +2,7 @@ import argparse
 
 import pandas as pd
 import torch
-import torch.nn.functional as F
 import torch.optim as optim
-from torch import nn
-from torch.optim.lr_scheduler import MultiStepLR
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
@@ -15,7 +12,6 @@ from model import Model
 
 # train for one epoch, each branch focus on different parts, to learn unique features
 def train(net, data_loader, train_optimizer):
-    global z
     net.train()
     total_loss, total_num, train_bar = 0.0, 0, tqdm(data_loader)
     for data, target, pos_index in train_bar:
@@ -23,41 +19,10 @@ def train(net, data_loader, train_optimizer):
         train_optimizer.zero_grad()
         features = net(data)
 
-        # randomly generate M+1 sample indexes for each batch ---> [B, M+1]
-        idx = torch.randint(high=n, size=(data.size(0), m + 1))
-        # make the first sample as positive
-        idx[:, 0] = pos_index
-        # select memory vectors from memory bank ---> [B, 1+M, E, D]
-        samples = torch.index_select(memory_bank, dim=0, index=idx.view(-1)) \
-            .view(data.size(0), -1, ensemble_size, feature_dim)
-        # compute cos similarity between each feature vector and memory bank ---> [B, E, 1+M]
-        sim_matrix = torch.bmm(samples.to(device=features.device).permute(0, 2, 1, 3).contiguous()
-                               .view(data.size(0) * ensemble_size, -1, feature_dim),
-                               features.view(data.size(0) * ensemble_size, -1).unsqueeze(dim=-1)) \
-            .view(data.size(0), ensemble_size, -1)
-        out = torch.exp(sim_matrix / temperature)
-        # Monte Carlo approximation ---> [1, E, 1], use the approximation derived from initial batches as z
-        if z is None:
-            z = out.detach().mean(dim=[0, 2], keepdim=True) * n
-        # compute P(i|v) ---> [B, E, 1+M]
-        output = out / z
-
         # compute loss
-        # compute log(h(i|v))=log(P(i|v)/(P(i|v)+M*P_n(i))) ---> [B, E]
-        p_d = (output.select(dim=-1, index=0) / (output.select(dim=-1, index=0) + m / n)).log()
-        # compute log(1-h(i|v'))=log(1-P(i|v')/(P(i|v')+M*P_n(i))) ---> [B, E, M]
-        p_n = ((m / n) / (output.narrow(dim=-1, start=1, length=m) + m / n)).log()
-        # compute J_NCE(θ)=-E(P_d)-M*E(P_n)
-        # TODO: Add branch nce loss
         loss = - (p_d.sum() + p_n.sum()) / (data.size(0) * ensemble_size)
         loss.backward()
         train_optimizer.step()
-
-        # update memory bank ---> [B, E, D]
-        pos_samples = samples.select(dim=1, index=0)
-        pos_samples = features.detach().cpu() * momentum + pos_samples * (1.0 - momentum)
-        pos_samples = F.normalize(pos_samples, dim=-1)
-        memory_bank.index_copy_(dim=0, index=pos_index, source=pos_samples)
 
         total_num += data.size(0)
         total_loss += loss.item() * data.size(0)
@@ -112,52 +77,30 @@ def test(net, memory_data_loader, test_data_loader):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Train MVC')
-    parser.add_argument('--model_type', default='resnet18', type=str,
-                        choices=['resnet18', 'resnet34', 'resnet50', 'resnext50_32x4d'], help='Backbone type')
-    parser.add_argument('--share_type', default='layer1', type=str,
-                        choices=['none', 'maxpool', 'layer1', 'layer2', 'layer3', 'layer4'], help='Shared module type')
-    parser.add_argument('--ensemble_size', default=8, type=int, help='Ensemble branch size')
-    parser.add_argument('--feature_dim', default=16, type=int, help='Feature dim for each branch')
-    parser.add_argument('--m', default=4096, type=int, help='Negative sample number')
+    parser.add_argument('--feature_dim', default=128, type=int, help='Feature dim for latent vector')
     parser.add_argument('--temperature', default=0.1, type=float, help='Temperature used in softmax')
-    parser.add_argument('--momentum', default=0.5, type=float, help='Momentum used for the update of memory bank')
-    parser.add_argument('--k', default=200, type=int, help='Top k most similar images used to predict the label')
-    parser.add_argument('--batch_size', default=128, type=int, help='Number of images in each mini-batch')
-    parser.add_argument('--epochs', default=200, type=int, help='Number of sweeps over the dataset to train')
-    parser.add_argument('--with_random', action='store_true', help='With branch random weight or not')
-    parser.add_argument('--gpu_ids', default='0,1,2,3,4,5,6,7', type=str, help='Selected gpu ids to use')
+    parser.add_argument('--batch_size', default=512, type=int, help='Number of images in each mini-batch')
+    parser.add_argument('--epochs', default=100, type=int, help='Number of sweeps over the dataset to train')
 
     # args parse
     args = parser.parse_args()
-    model_type, share_type, ensemble_size = args.model_type, args.share_type, args.ensemble_size
-    feature_dim, m, temperature = args.feature_dim, args.m, args.temperature
-    momentum, k, batch_size, epochs = args.momentum, args.k, args.batch_size, args.epochs
-    with_random, gpu_ids = args.with_random, [int(gpu) for gpu in args.gpu_ids.split(',')]
+    feature_dim, temperature, batch_size, epochs = args.feature_dim, args.temperature, args.batch_size, args.epochs
 
     # data prepare
     train_data = utils.CIFAR10Instance(root='data', train=True, transform=utils.train_transform, download=True)
-    train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=8)
+    train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=16)
     memory_data = utils.CIFAR10Instance(root='data', train=True, transform=utils.test_transform, download=True)
-    memory_loader = DataLoader(memory_data, batch_size=batch_size, shuffle=False, num_workers=8)
+    memory_loader = DataLoader(memory_data, batch_size=batch_size, shuffle=False, num_workers=16)
     test_data = utils.CIFAR10Instance(root='data', train=False, transform=utils.test_transform, download=True)
-    test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False, num_workers=8)
+    test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False, num_workers=16)
 
     # model setup and optimizer config
-    model = nn.DataParallel(Model(model_type, share_type, ensemble_size, feature_dim, with_random).to(gpu_ids[0]),
-                            device_ids=gpu_ids)
-    optimizer = optim.SGD(model.parameters(), lr=0.03, momentum=0.9, weight_decay=5e-4)
-    print("# trainable model parameters:", sum(param.numel() if param.requires_grad else 0
-                                               for param in model.parameters()))
-    lr_scheduler = MultiStepLR(optimizer, milestones=[int(epochs * 0.6), int(epochs * 0.8)], gamma=0.1)
-
-    # z as normalizer, init with None, c as num of train class, n as num of train data
-    z, c, n = None, len(memory_data.classes), len(train_data)
-    # init memory bank as unit random vector ---> [N, E, D]
-    memory_bank = F.normalize(torch.randn(n, ensemble_size, feature_dim), dim=-1)
+    model = Model(feature_dim).cuda()
+    optimizer = optim.Adam(model.parameters(), lr=1e-3)
 
     # training loop
     results = {'train_loss': [], 'test_acc@1': [], 'test_acc@5': []}
-    save_name_pre = 'cifar10_{}_{}_{}_{}_{}'.format(model_type, share_type, ensemble_size, feature_dim, with_random)
+    save_name_pre = '{}_{}_{}_{}'.format(feature_dim, temperature, batch_size, epochs)
     best_acc = 0.0
     for epoch in range(1, epochs + 1):
         train_loss = train(model, train_loader, optimizer)
@@ -167,8 +110,7 @@ if __name__ == '__main__':
         results['test_acc@5'].append(test_acc_5)
         # save statistics
         data_frame = pd.DataFrame(data=results, index=range(1, epoch + 1))
-        data_frame.to_csv('results/{}_features_extractor_results.csv'.format(save_name_pre), index_label='epoch')
-        lr_scheduler.step()
+        data_frame.to_csv('results/{}_statistics.csv'.format(save_name_pre), index_label='epoch')
         if test_acc_1 > best_acc:
             best_acc = test_acc_1
-            torch.save(model.module.state_dict(), 'epochs/{}_features_extractor.pth'.format(save_name_pre))
+            torch.save(model.state_dict(), 'results/{}_model.pth'.format(save_name_pre))

fec78a2720e777af4cdbf67b69f10c157802d4d9
leftthomas <leftthomas@qq.com>
2020-02-20 17:24:08 +0800

    finish test code

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torchvision.models.resnet import resnet18
 
 
@@ -20,4 +21,4 @@ class Model(nn.Module):
         x = self.f(x)
         feature = torch.flatten(x, start_dim=1)
         out = self.g(feature)
-        return feature, out
+        return F.normalize(feature, dim=-1), out
--- train.py
+++ train.py
@@ -37,34 +37,34 @@ def test(net, memory_data_loader, test_data_loader):
     total_top1, total_top5, total_num, feature_bank = 0.0, 0.0, 0, []
     with torch.no_grad():
         # generate feature bank
-        for data, target, _ in tqdm(memory_data_loader, desc='Feature extracting'):
-            feature_bank.append(net(data.to(gpu_ids[0])))
-        # [E, D, N]
-        feature_bank = torch.cat(feature_bank).permute(1, 2, 0).contiguous()
+        for data, _, target in tqdm(memory_data_loader, desc='Feature extracting'):
+            feature, out = net(data.cuda())
+            feature_bank.append(feature)
+        # [D, N]
+        feature_bank = torch.cat(feature_bank, dim=0).t().contiguous()
         # [N]
         feature_labels = torch.tensor(memory_data_loader.dataset.targets, device=feature_bank.device)
         # loop test data to predict the label by weighted knn search
         test_bar = tqdm(test_data_loader)
-        for data, target, _ in test_bar:
-            data, target = data.to(gpu_ids[0]), target.to(gpu_ids[0])
-            output = net(data)
+        for data, _, target in test_bar:
+            data, target = data.cuda(), target.cuda()
+            feature, out = net(data)
 
             total_num += data.size(0)
-            # compute cos similarity between each feature vector and feature bank ---> [E, B, N]
-            sim_matrix = torch.bmm(output.transpose(0, 1).contiguous(), feature_bank)
-            # [E, B, K]
-            sim_weight, sim_indices = sim_matrix.topk(k=k, dim=-1)
-            # [E, B, K]
-            sim_labels = torch.gather(feature_labels.expand(ensemble_size, data.size(0), -1), dim=-1, index=sim_indices)
+            # compute cos similarity between each feature vector and feature bank ---> [B, N]
+            sim_matrix = torch.mm(feature, feature_bank)
+            # [B, K]
+            sim_weight, sim_indices = sim_matrix.topk(k=200, dim=-1)
+            # [B, K]
+            sim_labels = torch.gather(feature_labels.expand(data.size(0), -1), dim=-1, index=sim_indices)
             sim_weight = (sim_weight / temperature).exp()
 
             # counts for each class
-            one_hot_label = torch.zeros(ensemble_size * data.size(0) * k, c, device=sim_labels.device)
-            # [E*B*K, C]
+            one_hot_label = torch.zeros(data.size(0) * 200, 10, device=sim_labels.device)
+            # [B*K, C]
             one_hot_label = one_hot_label.scatter(dim=-1, index=sim_labels.view(-1, 1), value=1.0)
             # weighted score ---> [B, C]
-            pred_scores = torch.sum(one_hot_label.view(ensemble_size, data.size(0), -1, c)
-                                    * sim_weight.unsqueeze(dim=-1), dim=[0, 2])
+            pred_scores = torch.sum(one_hot_label.view(data.size(0), -1, 10) * sim_weight.unsqueeze(dim=-1), dim=1)
 
             pred_labels = pred_scores.argsort(dim=-1, descending=True)
             total_top1 += torch.sum((pred_labels[:, :1] == target.unsqueeze(dim=-1)).any(dim=-1).float()).item()
